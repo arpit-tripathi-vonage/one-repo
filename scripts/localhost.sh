63eb306bf160e4d2117c474c9c06a7913a1b343f
Arpit Tripathi <arpit.tripathi@vonage.com>
2026-01-19 13:48:38 +0000

    local

---
+++ scripts/localhost.sh
@@ -0,0 +1,29 @@
+echo =========================================================
+echo Running $0 with $# arguments
+echo [processID] $$
+echo [arguments] $@
+echo =========================================================
+
+case $1 in
+  git)
+    echo =========================================================
+    echo "git fetch --all -p; git pull; git status;"
+    echo =========================================================
+    git fetch --all -p; git pull; git status;
+    ;;
+  start)
+    echo docker compose up
+    docker compose -f ./docker/docker-compose.yml up server -d
+    cmd=$( open -a "Google Chrome" http://localhost:9999/one-repo/ )
+    echo opening chrome $cmd
+    ;;
+  stop)
+    echo "running docker compose down"
+    docker compose -f ./docker/docker-compose.yml down
+    ;;
+  *)
+    echo use either --start or --stop
+    ;;
+esac
+
+echo
